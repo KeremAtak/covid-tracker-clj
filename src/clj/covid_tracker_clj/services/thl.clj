efce9a108aac2edd5156cd26ef1e132fc114979b
KeremAtak <kerem.atak@hotmail.com>
2021-01-09 16:30:24 +0200

    infections are mapped to municipalities

--- src/clj/covid_tracker_clj/services/thl.clj
+++ src/clj/covid_tracker_clj/services/thl.clj
@@ -4,11 +4,55 @@
             [covid-tracker-clj.config :refer [env]]))
 
 (defn curl-thl [address]
-  (->> 
-   address
-   get
-   :body
-   (m/decode "application/json")))
+  (->> address get :body (m/decode "application/json")))
+
+(defn insert-municipality [{:keys [infection-count municipality mapped-municipalities]}]
+  (let [keywordized-municipality (keyword municipality)]
+    (into mapped-municipalities {keywordized-municipality
+                                 {:statistics {:infections infection-count}}})))
+
+(defn municipalities-and-infections->mapped-municipalities
+  "Recursively returns a map which tells the total infection count of each municipality"
+  [{:keys [index infections municipalities mapped-municipalities]}]
+  (if (empty? index)
+    mapped-municipalities
+    (let [[index-kwd index-value] (first index)
+          keywordized-index-value (keyword (str index-value))
+          infection-count (keywordized-index-value infections)
+          municipality (index-kwd municipalities)
+          rest-index (rest index)
+          new-mapped-municipalities (insert-municipality {:index-value index-value
+                                                          :infection-count infection-count
+                                                          :municipality municipality
+                                                          :mapped-municipalities mapped-municipalities})]
+      (municipalities-and-infections->mapped-municipalities
+       {:index rest-index
+        :infections infections
+        :municipalities municipalities
+        :mapped-municipalities new-mapped-municipalities}))))
+
+(defn thl-cases->municipalities [thl-cases]
+  (->> thl-cases :dataset :dimension :hcdmunicipality2020 :category :label))
+
+(defn thl-cases->infections [thl-cases]
+  (->> thl-cases :dataset :value))
+
+(defn thl-cases->index [thl-cases]
+  (->> thl-cases :dataset :dimension :hcdmunicipality2020 :category :index))
+
+(defn thl-cases->mapped-municipalities [thl-cases]
+  (let [index (thl-cases->index thl-cases)
+        infections (thl-cases->infections thl-cases)
+        municipalities (thl-cases->municipalities thl-cases)
+        mapped-municipalities (municipalities-and-infections->mapped-municipalities
+                               {:index index
+                                :infections infections
+                                :municipalities municipalities
+                                :mapped-municipalities {}})]
+    mapped-municipalities))
 
 (defn get-thl-infections []
-  (curl-thl (:thl-url env)))
+  (let [thl-cases (curl-thl (:thl-infections env))
+        thl-deaths (curl-thl "https://sampo.thl.fi/pivot/prod/fi/epirapo/covid19case/fact_epirapo_covid19case.json?row=hcdmunicipality2020-445222&column=dateweek20200101-509030&filter=measure-492118")
+        mapped-municipalities (thl-cases->mapped-municipalities thl-cases)]
+    mapped-municipalities))
--- test/clj/covid_tracker_clj/test/services/thl.clj
+++ test/clj/covid_tracker_clj/test/services/thl.clj
@@ -11,7 +11,30 @@
     (f)))
 
 (deftest test-thl
-  (testing "curl from thl"
+  (testing "curl-thl"
     (let [test-url "https://sampo.thl.fi/pivot/prod/api/epirapo/covid19case.json"
           test-data (thl-service/curl-thl test-url)]
-          (is (contains? test-data :updated)))))
+      (is (contains? test-data :updated))))
+  (testing "thl-cases->mapped-municipalities"
+    (let [thl-cases {:dataset
+                     {:value {:10 "66"
+                              :16 "129"
+                              :8 "5012"
+                              :50 ".."}
+                      :dimension
+                      {:hcdmunicipality2020
+                       {:category
+                        {:index {:445177 10
+                                 :444182 8
+                                 :447912 16
+                                 :444111 50}
+                         :label {:445177 "Juva"
+                                 :444182 "Helsinki"
+                                 :444111 "Rääkkylä"
+                                 :447912 "Seinäjoki"}}}}}}
+          mapped-municipalities {:Juva {:statistics {:infections "66"}}
+                                 :Helsinki {:statistics {:infections "5012"}}
+                                 :Seinäjoki {:statistics {:infections "129"}}
+                                 :Rääkkylä {:statistics {:infections ".."}}}]
+      (is (= mapped-municipalities
+             (thl-service/thl-cases->mapped-municipalities thl-cases))))))
